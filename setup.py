ebfa2b13c180696a8fa4fe5b7154248977852e21
michaelborchert <mborch@amazon.com>
2017-11-17 14:50:12 +0800

    Bumping package version for pypi upload.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readme():
         return f.read()
 
 setup(name='rdk',
-      version='0.2.25',
+      version='0.2.26',
       description='Rule Development Kit CLI for AWS Config',
       long_description=readme(),
       url='https://github.com/michaelborchert/rdk',
